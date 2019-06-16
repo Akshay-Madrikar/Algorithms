3a1d6415a1a91d9ab669e8cb0d684477f8815513
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-16 19:12:14 +0530

    Algorithm updated

---
+++ allLongestStrings/allLongestStrings.ts
@@ -0,0 +1,19 @@
+function allLongestStrings(inputString: string[]): string[] {
+    let longestLength = 0
+    const longestWord = []
+
+    inputString.forEach((word) => {
+        longestLength = longestLength < word.length ? word.length : longestLength
+    })
+
+    inputString.forEach((word) => {
+        if (word.length === longestLength) {
+            longestWord.push(word)
+        }
+    })
+
+    return longestWord
+}
+
+
+console.log(allLongestStrings(['akshay', 'abcefg', 'a', 'b', 'bc']))
