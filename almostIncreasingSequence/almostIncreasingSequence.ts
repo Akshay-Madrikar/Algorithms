5db8e9de0f4d1ac10bb33ff67c6c32bef9665984
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-17 20:38:11 +0530

    Algorithm updated

---
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -0,0 +1,16 @@
+function almostIncreasingSequence(sequence: number[]): boolean {
+    let count = 0
+    for (let i = 0; i < sequence.length; i++) {
+        if (sequence[i] <= sequence[i - 1]) {
+            count++
+            if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+                return false
+            }
+        }
+    }
+    return count <= 1;
+}
+
+
+console.log(almostIncreasingSequence([3, 1, 2, 4]))
+console.log(almostIncreasingSequence([1, 6, 5, 4]))
