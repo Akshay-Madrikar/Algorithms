a1fd0b243868dd63acb90cdd89ec23b12978ae80
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-19 19:02:44 +0530

    Algorithm updated

--- add/add.js
+++ add/add.js
@@ -1,17 +1,17 @@
-function add(num1, num2) {
+function add ( num1, num2 ) {
     const sum = num1 + num2;
     return sum;
 }
 
-function add2(...numbers) {
+function add2 ( ...numbers ) {
     let total = 0;
 
-    numbers.forEach((num) => {
+    numbers.forEach( ( num ) => {
         total += num;
-    })
+    } )
 
     return total;
 }
 
-console.log(add(2, 4)); //Output = 6
-console.log(add2(2, 5, 9, 12)); //Output = 28
+console.log( add( 2, 4 ) ); //Output = 6
+console.log( add2( 2, 5, 9, 12 ) ); //Output = 28
