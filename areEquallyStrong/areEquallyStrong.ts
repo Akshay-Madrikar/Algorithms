9616b1c59a280b61813ef2d02ba3e36cbf5c248a
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-21 20:26:31 +0530

    Algorithm updated

---
+++ areEquallyStrong/areEquallyStrong.ts
@@ -0,0 +1,12 @@
+function areEquallyStorng(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+
+    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight
+    const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight
+    const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight
+    const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight
+
+    return yourWeakest === friendsWeakest && yourStrongest === friendsStrongest
+}
+
+console.log(areEquallyStorng(15, 10, 15, 10))
+console.log(areEquallyStorng(10, 15, 15, 9))
