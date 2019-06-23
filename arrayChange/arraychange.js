f5d059e6d73d2984b45ec1bfeb83bae6794256b8
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-23 18:06:37 +0530

    Algorithm updated

---
+++ arrayChange/arraychange.js
@@ -0,0 +1,15 @@
+const arrayChange = ( arr ) => {
+    let count = 0
+
+    for ( let i = 0; i < arr.length; i++ ) {
+        if ( arr[ i ] >= arr[ i + 1 ] ) {
+            const diff = ( arr[ i ] + 1 ) - arr[ i + 1 ]
+            arr[ i + 1 ] = arr[ i ] + 1
+            count = count + diff
+        }
+    }
+    return count
+}
+
+console.log( arrayChange( [ 1, 1, 1 ] ) )
+console.log( arrayChange( [ 2, 2, 1 ] ) )
