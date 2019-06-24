54d69066cb02458b30bb263f838118791832a442
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-24 20:32:40 +0530

    Algorithm updated

---
+++ arrayConversion/arrayConversion.ts
@@ -0,0 +1,30 @@
+function arrayConversion(arr: number[]): number {
+    let isOdd = true
+
+    while (arr.length !== 1) {
+        arr = sumProduct(arr, isOdd)
+        isOdd = !isOdd
+    }
+    return arr[0]
+}
+
+function sumProduct(numbers: number[], isOdd: boolean): number[] {
+
+    const sumProducts: number[] = []
+
+    if (isOdd) {
+        for (let i = 0; i < numbers.length; i += 2) {
+            sumProducts.push(numbers[i] + numbers[i + 1])
+        }
+    } else {
+        for (let i = 0; i < numbers.length; i += 2) {
+            sumProducts.push(numbers[i] * numbers[i + 1])
+        }
+    }
+    return sumProducts
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]))
+console.log(arrayConversion([1, 4, 6, 8]))
+
+
