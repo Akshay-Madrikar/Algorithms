ed4762fafedc1cd9dc583698ace41f787b021a54
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-25 11:49:38 +0530

    Algorithm updated

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,11 +1,23 @@
 function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
     let sum = 0
-    for (let i = 0; i < inputArray.length; i++) {
-        inputArray[i] = inputArray[i] + inputArray[i + 1]
+    let max = 0
+
+    for (let i = 0; i < k; i++) {
         sum += inputArray[i]
     }
 
-    return sum
+    max = sum
+
+    for (let i = k; i < inputArray.length; i++) {
+        sum -= inputArray[i - k]
+        sum += inputArray[i]
+
+        if (sum > max) {
+            max = sum
+        }
+    }
+    return max
 }
 
 console.log(arrayMaxConsecutiveSum([1, 2, 3, 4, 5], 2))
+console.log(arrayMaxConsecutiveSum([1, 2, 3, 4, 5], 3))
