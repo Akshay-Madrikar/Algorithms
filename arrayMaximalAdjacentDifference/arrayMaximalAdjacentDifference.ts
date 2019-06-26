be69351ea2644daf9bc33ed0bc81a98994ddabb9
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-26 18:11:04 +0530

    Algorithm updated

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,13 +1,13 @@
 function arrayMaximalAdjacentDifference(inputArray: number[]): number {
 
-    let maxDiff = inputArray[0] - inputArray[1]
+    let maxDiff = Math.abs(inputArray[0] - inputArray[1])
 
     for (let i = 0; i < inputArray.length; i++) {
-        const diff = inputArray[i - 1] - inputArray[i]
+        const diff = Math.abs(inputArray[i - 1] - inputArray[i])
         maxDiff = diff > maxDiff ? diff : maxDiff
     }
     return maxDiff
 }
 
-console.log(arrayMaximalAdjacentDifference([1, 3, 4, 8]))
-console.log(arrayMaximalAdjacentDifference([1, 3, 12, 8]))
+console.log(arrayMaximalAdjacentDifference([1, 3, 4, 8])) //Output : 4
+console.log(arrayMaximalAdjacentDifference([1, 3, 12, 8])) //Output : 9
