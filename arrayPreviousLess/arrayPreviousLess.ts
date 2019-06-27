6334f03b78a24e9dee6edf5ca32a06f9e42335be
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-27 15:40:22 +0530

    Algorithm updated

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,7 +1,19 @@
 function arrayPreviousLess(inputArray: number[]): number[] {
 
+    let lessThanArray: number[] = []
+    for (let i = inputArray.length - 1; i >= 0; i--) {
+        for (let j = i; j >= 0; j--) {
+            if (inputArray[i] > inputArray[j]) {
+                lessThanArray.unshift(inputArray[j])
+                break
+            } else if (j === 0) {
+                lessThanArray.unshift(-1)
+            }
+        }
+    }
 
-    return
+    return lessThanArray
 }
 
-console.log(arrayPreviousLess([3, 5, 2, 4, 5]))
+console.log(arrayPreviousLess([3, 5, 2, 4, 5])) //Output : [-1, 3,- 1, 2, 4]
+console.log(arrayPreviousLess([2, 1, 5, 7, 8])) //output : [-1, -1, 1, 5, 7]
