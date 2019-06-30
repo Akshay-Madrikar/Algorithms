81f7fb8b2eaef443b433fa5d4e6b71038ace6c1e
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-06-30 16:28:36 +0530

    Algorithm updated

---
+++ avoidObstacles/avoidObstacles.ts
@@ -0,0 +1,15 @@
+function avoidObstacles(inputArray: number[]): number {
+
+    inputArray = inputArray.sort((a, b) => a - b)
+
+    const largestArrayVal = inputArray[inputArray.length - 1]
+
+    for (let i = 1; i <= largestArrayVal + 1; i++) {
+        if (inputArray.every((element) => element % i !== 0)) {
+            return i
+        }
+    }
+
+}
+
+console.log(avoidObstacles([4, 5, 3, 1, 2]))
