65f333b1144b2bec2221a2fb969c82024b351414
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-07-03 21:51:15 +0530

    Algorithm updated

--- caseInsensitivePalindrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalindrome/caseInsensitivePalindrome.ts
@@ -1,7 +1,11 @@
 function caseInsensitivePalindrome(inputString: string): boolean {
 
+    let ogLowerCase = inputString.toLowerCase()
+    let reverseString = ogLowerCase.split('').reverse().join('')
 
-    return
+    return ogLowerCase === reverseString
+
 }
 
 console.log(caseInsensitivePalindrome('AaBaa'))
+console.log(caseInsensitivePalindrome('ajax'))
