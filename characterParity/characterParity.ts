30949771bd399494850aed82e64cc6bf719d5bec
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-07-05 23:56:34 +0530

    Algorithm updated

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,15 @@
 function characterParity(inputCharacter: any): string {
 
 
-    return
+    if (isNaN(inputCharacter)) {
+        return 'Not a digit'
+    } else if ((inputCharacter % 2 === 0)) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+
 }
 
-console.log(characterParity(8))
+console.log(characterParity('8'))
+console.log(characterParity('100F'))
