e89697d6fed03b5ce2248bdcdd5c8f81cf19a43b
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-07-07 17:00:17 +0530

    Algorithm updated

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -10,5 +10,5 @@ function chunkyMonkey(inputArray: any[], size: number): any[][] {
 
 }
 
-console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2))
-console.log(chunkyMonkey([1, 2, 3, 4, 5], 3))
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2)) //Output [ [ 'a', 'b' ],[ 'c', 'd' ] ]
+console.log(chunkyMonkey([1, 2, 3, 4, 5], 3)) //Output [ [ 1, 2, 3 ],[ 4, 5 ] ]
