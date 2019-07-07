db7f288a454d7a7b2ea759ee9026dcb3f4feb305
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-07-07 16:58:53 +0530

    Algorithm updated

---
+++ chunkyMonkey/chunkyMonkey.ts
@@ -0,0 +1,14 @@
+function chunkyMonkey(inputArray: any[], size: number): any[][] {
+
+    let newArray = []
+    let count = 0
+
+    while (count < inputArray.length) {
+        newArray.push(inputArray.slice(count, count += size))
+    }
+    return newArray
+
+}
+
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2))
+console.log(chunkyMonkey([1, 2, 3, 4, 5], 3))
