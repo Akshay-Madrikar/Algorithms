2c777b414869dabd2dab424a87812309210e601f
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-07-21 15:48:32 +0530

    Algorithm updated

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -2,6 +2,6 @@ function celsiusToFahrenheit(celcius: number): number {
     return (celcius * 9 / 5) + 32
 }
 
-console.log(celsiusToFahrenheit(-30))
-console.log(celsiusToFahrenheit(-10))
-console.log(celsiusToFahrenheit(0))
+console.log(celsiusToFahrenheit(-30)) //Output : -22
+console.log(celsiusToFahrenheit(-10)) //Output : 14
+console.log(celsiusToFahrenheit(0)) //Output : 32
