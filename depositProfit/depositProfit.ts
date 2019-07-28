7b6cc56eee40790cdc9652b3df09fa3a5955d264
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-07-28 16:18:46 +0530

    Algorithm updated

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,1 +1,13 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number
+function depositProfit(deposit: number, rate: number, threshold: number): number {
+
+    let year = 0
+
+    while (threshold > deposit) {
+        deposit = deposit + (deposit * rate / 100)
+        year++
+    }
+    return year
+}
+
+console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 200))
