94d4f552b2ae08f80b647f4e51dc9cc1204cbbbc
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-08-02 23:56:08 +0530

    Algorithm updated

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,6 +1,13 @@
 function differentSymbolsNaive(inputString: String): number {
+    let uniqueCharacter: string[] = []
+    const alphabets: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    return
+    let s1 = inputString.split('')
+    if (alphabets.includes(s1)) {
+
+
+    }
+    return s1
 }
 
 console.log(differentSymbolsNaive('cabca'))
