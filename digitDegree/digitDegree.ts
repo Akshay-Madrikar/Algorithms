427d0a098e81d863619dea87547bd87c71e02c43
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-08-04 14:56:17 +0530

    Algorithm updated

---
+++ digitDegree/digitDegree.ts
@@ -0,0 +1,30 @@
+function digitDegree(n: number): number {
+    let count = 0
+    let currentNumber = n
+
+    if (n <= 9) {
+        return count
+    } else {
+        while (true) {
+            count++
+            currentNumber = getDigit(currentNumber)
+
+            if (currentNumber <= 9) {
+                break
+            }
+        }
+    }
+    return count
+}
+
+function getDigit(num: number) {
+    const nums: number[] = num.toString().split('').map((el) => parseInt(el))
+
+    return nums.reduce((a, b) => {
+        return a + b
+    })
+}
+
+console.log(digitDegree(5))
+console.log(digitDegree(100))
+console.log(digitDegree(91))
