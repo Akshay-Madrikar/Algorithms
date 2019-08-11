c3cc0fbee4a2e2f729ee80f59af5b48a31eeb9d7
Akshay-Madrikar <akshaymadrikar@gmail.com>
2019-08-11 15:51:35 +0530

    Algorithm updated

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,7 +1,19 @@
-function electionWinners(a: number[], k: number): number {
+function electionWinners(votes: number[], k: number): number {
+    let result = 0
+    const maxVotes = Math.max(...votes)
+    const sortedVotes = votes.sort((a, b) => b - a)
 
+    if (sortedVotes[0] !== sortedVotes[1] && k === 0) {
+        return 1
+    }
 
-    return
+    votes.forEach((vote) => {
+        if (maxVotes - vote < k) {
+            result++
+        }
+    })
+
+    return result
 }
 
-console.log(electionWinners([2, 3, 5, 2], 2))
+console.log(electionWinners([2, 3, 5, 2], 3))
